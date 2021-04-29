517620c1ffcf7e2f33b321f827ea8e038e080489
Alessandro Molinari <alessandromolinari2000@hotmail.com>
2021-04-29 17:04:24 +0200

    Add files via upload
    Final commit

--- hangman_2.py
+++ hangman_2.py
@@ -1,70 +1,11 @@
 
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
+import stages as stages
+from logo import logo
+from hangman_words import word_list
 import random
 
-words = [
-        "papera",
-        "palla",
-        "banana"
-        ]
-
+words = word_list
+print(logo)
 lives = 6
 game_over = False
 parola_da_indovinare = random.choice(words)
@@ -83,7 +24,8 @@ while not game_over:
             display[position] = tentativo
     if display[position] != tentativo:
         lives -= 1
-        print(stages[lives])
+        print(stages.stages[lives])
+        print(f"Sorry, letter '{tentativo}' does not belong to the word.")
         if lives == 0:
             game_over = True
             print("You lost")
